fd5170630960e625c71bb12e33293e2632d5a91b
José Carlos de Souza <jcjrta@gmail.com>
2024-02-26 22:37:47 -0300

    Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export default function Header(){
+
+    return(
+        <header>
+            <div>
+                <h1>DSCarrosTop</h1>
+            </div>
+            
+
+        </header>
+
+    );
+}
